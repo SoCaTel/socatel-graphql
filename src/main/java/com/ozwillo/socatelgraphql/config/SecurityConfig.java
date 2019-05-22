446f0492d1606b27c4cc0667deb122d5a53336c7
Benoit Orihuela <borihuela@ozwillo.org>
2019-05-22 19:01:47 +0200

    feat: activate health actuator endpoint

--- src/main/java/com/ozwillo/socatelgraphql/config/SecurityConfig.java
+++ src/main/java/com/ozwillo/socatelgraphql/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.ozwillo.socatelgraphql.config;
 
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.boot.actuate.health.Health;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,7 +12,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic().and()
-                .authorizeRequests().anyRequest().fullyAuthenticated().and()
+                .requestMatcher(EndpointRequest.to(Health.class)).authorizeRequests().anyRequest().permitAll().and()
+                .antMatcher("/graphql").authorizeRequests().anyRequest().fullyAuthenticated().and()
                 .csrf().disable();
     }
 }
